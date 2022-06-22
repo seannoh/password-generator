b85658308d4d460478331405c31aa64b694d3612
Sean Oh <seanoh@ucsb.edu>
2022-06-21 22:36:27 -0700

    add prompt for length of password

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Generate password based on a series of prompts to the user
+function generatePassword() {
+  var characters = "";
+  var passwordLength = window.prompt("How many characters would you like your password to be?");
+  if(passwordLength < 8){
+    window.alert("Password must be at least 8 characters long!");
+    return;
+  }else if(passwordLength > 128){
+    window.alert("Password must be at most 128 characters long!");
+    return;
+  }
+  
+}
